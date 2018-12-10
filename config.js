148e5639de545fff5f5f050905bc92c01329275e
marcos <marcosfreire.dev@gmail.com>
2018-12-10 16:34:15 -0300

    vai!!! porra"

---
+++ config.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyD13OIMv7p6Qasybm78jy-2wjWxc-LNg-k",
+    authDomain: "amfi-d3925.firebaseapp.com",
+    databaseURL: "https://amfi-d3925.firebaseio.com",
+    projectId: "amfi-d3925",
+    storageBucket: "amfi-d3925.appspot.com",
+    messagingSenderId: "719855208861"
+  };
---
+++ todo.js
@@ -0,0 +1,39 @@
+
+
+
+let tarefas = [];
+
+
+function addTarefa(){
+
+    let tarefa = document.getElementById("inputTodo").value;
+
+    tarefas.push(tarefa);
+
+/*
+    tarefas.push({
+        'tarefa': 'tarefa 1'
+    });
+
+    tarefas.push({
+        tarefa: 'tarefa 2'
+    });
+
+
+
+    for(let i = 0; i < tarefas.length; i++){
+
+        if(tarefas[i]){
+            //tarefas[i].push(tarefa);
+            console.log(tarefas[i]);
+        }
+    }
+
+*/
+
+    document.getElementById("inputTodo").value = '';
+
+
+
+}
+
